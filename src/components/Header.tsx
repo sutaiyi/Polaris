c9c87e186f57822c5384bbb73a08c487c7713365
sutaiyi <646404162@qq.com>
2023-12-20 01:56:10 +0800

    hh

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,10 @@
 import Brightness4Icon from "@mui/icons-material/Brightness4";
 import Brightness7Icon from "@mui/icons-material/Brightness7";
 import { AppBar, Box, IconButton, useTheme } from "@mui/material";
-import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useContext } from "react";
 
-import logo from "@/assets/imgs/logo.png";
 import ColorModeContext from "@/contexts/colorModeContext";
 
 export default function Header() {
@@ -31,12 +29,12 @@ export default function Header() {
       className=" flex h-16 flex-row items-center justify-between px-10"
     >
       <h1 className=" flex items-center gap-2 text-3xl font-bold">
-        <Image
+        {/* <Image
           src={logo}
           alt="logo"
           width={40}
           height={40}
-        />
+        /> */}
         <Link href="/">Polaris</Link>
       </h1>
 
